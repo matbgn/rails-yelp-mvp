11d2a37ad282a953c3ff647c6e52f948cf31b739
Matthieu Borgognon <matrash@bqn.ch>
2019-05-15 14:45:18 +0200

    Added restaurant_id migration and final validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_122634) do
+ActiveRecord::Schema.define(version: 2019_05_15_123911) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_05_15_122634) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
   end
 
 end

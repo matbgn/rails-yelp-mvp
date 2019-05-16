9c6a030c695196dc8d4e9226836d870aa1fa41d3
Matthieu Borgognon <matrash@bqn.ch>
2019-05-16 10:20:32 +0200

    TODO: Error in db schema about referencs in review model

--- db/migrate/20190515140038_change_referencing_to_restaurant.rb
+++ db/migrate/20190515140038_change_referencing_to_restaurant.rb
@@ -1,11 +1,7 @@
 class ChangeReferencingToRestaurant < ActiveRecord::Migration[5.2]
   def change
     change_table :reviews do |t|
-        t.string :content
-        t.integer :rating
-        t.datetime :created_at, null: false
-        t.datetime :updated_at, null: false
-        t.rename :restaurant, :restaurant_id
+        t.rename "restaurant", "restaurant_id"
     end
   end
 end
